850265595bd3a9da6fbd3110640105e0c50ad3ba
Rhys Morgan <rhysm@me.com>
2022-07-21 10:26:27 +0100

    Initial Commit

---
+++ BackportAttributeGraph/BackportAttributeGraphApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackportAttributeGraphApp.swift
+//  BackportAttributeGraph
+//
+//  Created by Rhys Morgan on 21/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct BackportAttributeGraphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
